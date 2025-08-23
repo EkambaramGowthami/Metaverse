2804432022ee99869e8bf0e25081151b06633c00
Ekambaram Gowthami <ekambaramgowthami759@gmail.com>
2025-08-23 18:21:46 +0530

    Update TileMap.jsx

--- frontend/src/maps/TileMap.jsx
+++ frontend/src/maps/TileMap.jsx
@@ -254,7 +254,7 @@
 import React, { useEffect, useRef } from 'react';
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
+const socket = io("https://metaverse-3joe.onrender.com", {
   withCredentials: true,
   transports: ["websocket"]
 });
