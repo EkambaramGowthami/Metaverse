0bf6cbcf03d09da66bab0b0a518f23792a9d1491
Gowthami <ekambaramgowthami759@gmail.com>
2025-08-23 21:45:46 +0530

    removed const and added function in functio defination of the test route

--- frontend/src/components/Test.jsx
+++ frontend/src/components/Test.jsx
@@ -1,4 +1,4 @@
-export const Test = () =>{
+export default function Test (){
     return (
         <div className="flex justify-center items-center bg-red-500 text-black text-2xl">
             <p>hit there</p>
