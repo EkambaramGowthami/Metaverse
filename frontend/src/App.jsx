3fbdee611ad7dc5bc6be21876bc7bf6cd7d635b3
Gowthami <ekambaramgowthami759@gmail.com>
2025-08-23 21:07:23 +0530

    testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { DisplayMap } from './components/DisplayMap';
 import { Dashboard } from './components/Dashboard';
 import { MyGallery } from './components/Space/MyGallery';
 import Signup from './components/Auth/Signup';
+import { Test } from './components/Test';
 
 
 
@@ -15,6 +16,7 @@ export default function App() {
       <Route path="/space" element={<MyGallery />} />
       <Route path="/space/room/:roomId" element={<DisplayMap />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/test" element={<Test />} />
       
      
 
---
+++ frontend/src/components/Test.jsx
@@ -0,0 +1,10 @@
+export const Test = () =>{
+    return (
+        <div className="flex justify-center items-center bg-red-500 text-black text-2xl">
+            <p>hit there</p>
+            <button className="px-12 py-4 bg-green-500 text-white">hi there</button>
+
+
+        </div>
+    )
+}
