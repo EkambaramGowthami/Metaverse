eec51eaa74374c255955d130259ef057887e5e14
Gowthami <ekambaramgowthami759@gmail.com>
2025-08-31 21:40:26 +0530

    changed the vercel.json and vite.config.js files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import  DisplayMap  from './components/DisplayMap';
 import  MyGallery  from './components/Space/MyGallery';
 import Signup from './components/Auth/Signup';
@@ -13,14 +13,14 @@ import ZegoVideoConference from './components/video/VideoCallPage';
 export default function App() {
   const [players, setPlayers] = useState([]);
     return (
-    <HashRouter>
+    <BrowserRouter>
     <Routes >
       <Route path="/space" element={<MyGallery players={players} setPlayers={setPlayers} />} />
       <Route path="/space/room/:roomId" element={<DisplayMap  players={players} setPlayers={setPlayers}/>} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/videocall" element={<ZegoVideoConference />} />
        </Routes>
-  </HashRouter>
+  </BrowserRouter>
 
   );
   
