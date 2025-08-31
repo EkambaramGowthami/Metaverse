e1eaac4c5cd2037cdeeec7168fa57ca4a71a09aa
Gowthami <ekambaramgowthami759@gmail.com>
2025-08-31 21:11:59 +0530

    deleted the unwanted file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import  DisplayMap  from './components/DisplayMap';
 import  MyGallery  from './components/Space/MyGallery';
 import Signup from './components/Auth/Signup';
-import Test  from './components/Test';
-import Hi from './components/video/Hi';
+import ZegoVideoConference from './components/video/VideoCallPage';
+
 
 
 
@@ -14,18 +14,12 @@ export default function App() {
   const [players, setPlayers] = useState([]);
     return (
     <BrowserRouter basename="/">
-    <Routes path="/">
+    <Routes >
       <Route path="/space" element={<MyGallery players={players} setPlayers={setPlayers} />} />
       <Route path="/space/room/:roomId" element={<DisplayMap  players={players} setPlayers={setPlayers}/>} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/test" element={<Test />} />
-     <Route path="/hi" element={<Hi />} />
-      <Route path="/videocall" element={<Test />} />
-      <Route path="*" element={<h1>404 Not Found</h1>} />
-      
-     
-
-    </Routes>
+      <Route path="/videocall" element={<ZegoVideoConference />} />
+       </Routes>
   </BrowserRouter>
 
   );
