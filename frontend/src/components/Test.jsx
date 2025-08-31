2c054bc9c7271d3407c4dbdc30c94e75c0ad6149
Gowthami <ekambaramgowthami759@gmail.com>
2025-08-31 09:14:45 +0530

    added teh some code in the tile.jsx file

--- frontend/src/components/Test.jsx
+++ frontend/src/components/Test.jsx
@@ -1,8 +1,12 @@
+import { useNavigate } from "react-router-dom"
+
 export default function Test (){
+    const navigate = useNavigate();
     return (
         <div className="flex justify-center items-center bg-red-500 text-black text-2xl">
             <p>hit there</p>
             <button className="px-12 py-4 bg-green-500 text-white">hi there</button>
+            <button className="px-12 py-4 bg-green-500 text-white" onClick={() => navigate("/videocall")}>click here to go to the videocall page</button>
 
 
         </div>
