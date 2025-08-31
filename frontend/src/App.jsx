f9c7f3d6f76cfc2775aefb0c652769ee537468f1
Gowthami <ekambaramgowthami759@gmail.com>
2025-08-31 20:39:27 +0530

    changed the main.jsx file and deleted the unwanted imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import  DisplayMap  from './components/DisplayMap';
-import  Dashboard  from './components/Dashboard';
 import  MyGallery  from './components/Space/MyGallery';
 import Signup from './components/Auth/Signup';
 import Test  from './components/Test';
