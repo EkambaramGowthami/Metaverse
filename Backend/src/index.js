e212a7564c4289f58db0e8cb35521d57e0ebc3b7
Gowthami <ekambaramgowthami759@gmail.com>
2025-09-01 20:22:29 +0530

    changes the cors origin

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -19,10 +19,10 @@ const appId = 1472471415;
 const serverSecret = "82938042ac4a8914744e6de0b58e602d";
 
 
-app.use(cors({ origin: ["metaverse-5dvvqyz8g-gowthamis-projects-b7f16ceb.vercel.app", "http://localhost:5173"], origin: true, credentials: true, methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], allowedHeaders: ["Content-Type", "Authorization"] })); const httpServer = http.createServer(app); 
+app.use(cors({ origin: ["https://metaverse-osl5wuy6d-gowthamis-projects-b7f16ceb.vercel.app", "http://localhost:5173"], origin: true, credentials: true, methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], allowedHeaders: ["Content-Type", "Authorization"] })); const httpServer = http.createServer(app); 
 
 
-const io = new Server(httpServer, { cors: { origin: ["metaverse-5dvvqyz8g-gowthamis-projects-b7f16ceb.vercel.app", "http://localhost:5173"], origin: true, credentials: true }, });
+const io = new Server(httpServer, { cors: { origin: ["https://metaverse-osl5wuy6d-gowthamis-projects-b7f16ceb.vercel.app", "http://localhost:5173"], origin: true, credentials: true }, });
 
 app.post("/signup", async (req, res) => {
     try {
