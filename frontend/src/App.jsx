629f179c9d1f62dc87596e3b5e31b48550220ec5
Gowthami <ekambaramgowthami759@gmail.com>
2025-09-01 08:27:40 +0530

    chnaged the BrowserRouter to HashRouter and also changed the vercel.json

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import  DisplayMap  from './components/DisplayMap';
 import  MyGallery  from './components/Space/MyGallery';
 import Signup from './components/Auth/Signup';
@@ -7,14 +7,14 @@ import VideoCallPage from './components/video/VideoCallPage';
 export default function App() {
   const [players, setPlayers] = useState([]);
     return (
-    <BrowserRouter>
+    <Router>
     <Routes >
       <Route path="/space" element={<MyGallery players={players} setPlayers={setPlayers} />} />
       <Route path="/space/room/:roomId" element={<DisplayMap  players={players} setPlayers={setPlayers}/>} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/videocall" element={<VideoCallPage />} />
     </Routes>
-  </BrowserRouter>
+  </Router>
 
   );
   
