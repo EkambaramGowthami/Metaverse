486b33f0c48909ba570459e15dc1ad44bc4391c4
Gowthami <ekambaramgowthami759@gmail.com>
2025-09-01 13:55:58 +0530

    added the test file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import  DisplayMap  from './components/DisplayMap';
 import  MyGallery  from './components/Space/MyGallery';
 import Signup from './components/Auth/Signup';
+import Test from './components/video/Test';
 export default function App() {
   const [players, setPlayers] = useState([]);
     return (
-    <BrowserRouter>
-    <Routes >
+  <BrowserRouter>
+    <Routes>
       <Route path="/space" element={<MyGallery players={players} setPlayers={setPlayers} />} />
       <Route path="/space/room/:roomId" element={<DisplayMap  players={players} setPlayers={setPlayers}/>} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/test" element={<Test />} />
       <Route path="/videocall" element={<div>Video Call Route Works!</div>} />
     </Routes>
   </BrowserRouter>
