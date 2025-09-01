234466ffdcdcc01a0e3854898dfca31ca0fc97b3
Gowthami <ekambaramgowthami759@gmail.com>
2025-09-01 21:03:53 +0530

    added the polling in the transports

--- frontend/src/components/utils/socket.jsx
+++ frontend/src/components/utils/socket.jsx
@@ -2,5 +2,6 @@ import { io } from "socket.io-client";
 const BackendUrl = import.meta.env.VITE_BACKEND_URL;
 export const socket = io(`${BackendUrl}`, {
   withCredentials: true,
-  transports: ["websocket"],
+  transports: ["websocket","polling"],
+  reconnection: true,
 });
